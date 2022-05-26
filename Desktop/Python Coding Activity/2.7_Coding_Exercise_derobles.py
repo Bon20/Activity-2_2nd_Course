159a9c265cf8f3806616d8b42e360913cfef828c
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-05-27 05:05:53 +0700

    Module 2.7

---
+++ Desktop/Python Coding Activity/2.7_Coding_Exercise_derobles.py
@@ -0,0 +1,6 @@
+Name = input("Enter your name here: ")
+Age = int(input ("How old are you?: "))
+Favorite_color = input("What is your favorite color?: ")
+Favorite_movie = input("What is your favorite Movie?: ")
+Mobile_number = int(input ("Mobile Number: "))
+Motto_in_life = input("What is you motto in life?: ")
