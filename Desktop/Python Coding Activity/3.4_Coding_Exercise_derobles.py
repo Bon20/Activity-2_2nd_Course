9bbbd18d5638e761e5849c33481368a07b46758c
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-05-27 05:07:02 +0700

    Module 3.4

---
+++ Desktop/Python Coding Activity/3.4_Coding_Exercise_derobles.py
@@ -0,0 +1,21 @@
+class Customers:
+    greeting = "Welcome to the Coffee Place!"
+
+print(Customers.greeting)
+
+c_1= Customers()
+c_1.Name = "Samirah"
+c_1.Beverage = "Iced caffe latte"
+c_1.Food = "Cinnamon roll"
+c_1.Total = 225
+
+print(c_1.Beverage)
+
+
+c_2= Customers()
+c_2.Name = "Jerry"
+c_2.Beverage = "Caramel macchiato"
+c_2.Food = "Glazed doughnut"
+c_2.Total = 230
+
+print(c_2.Food)
