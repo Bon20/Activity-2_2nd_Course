4e16980646e8e337e4d9a68482adb31e5c3ae137
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-05-28 01:28:35 +0700

    Module 4.4

---
+++ Desktop/Course 2 Coding Exercise/Course2_Module 4.4/functionfile.py
@@ -0,0 +1,10 @@
+def your_name(name):
+    print("My name is " + name + "!")
+def your_location(location):
+    print("This " + location + "is nice")
+def your_age(age):
+    print("Im " + age + "years old")
+def your_hobby(hobby):
+    print(hobby +" is my hobby")
+def favorite_food(favorite_food):
+    print(favorite_food + "is my favorite food")
---
+++ Desktop/Course 2 Coding Exercise/Course2_Module 4.4/main.py
@@ -0,0 +1,5 @@
+from functionfile import your_name
+from functionfile import your_age
+
+your_name("Bon")
+your_age("24")
